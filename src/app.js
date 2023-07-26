b2287d3e43548f78ef68e7e7abbfd1371322d8a6
Samuell <samuelltorres145@gmail.com>
2023-07-26 17:51:12 -0300

    modified cors

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ import cors from 'cors'
 import './database'
 
 const corsOptions = {
-  origin: 'https://codeburgerinterface.vercel.app/',
+  origin: 'https://codeburgerinterface.vercel.app',
   credentials: true,
 }
 
